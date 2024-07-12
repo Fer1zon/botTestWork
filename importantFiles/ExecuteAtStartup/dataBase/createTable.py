3952e84f52c9d49f6c321ffb6da8ee32f99cd277
Fer1zon <maksimbegisev119@gmail.com>
2024-07-12 13:16:59 +0400

    Создал таблицы

--- importantFiles/ExecuteAtStartup/dataBase/createTable.py
+++ importantFiles/ExecuteAtStartup/dataBase/createTable.py
@@ -6,15 +6,28 @@ from ...helps import cur, conn
 
 def createUserTable():#Создание юзерских таблиц
 
-    cur.execute("""CREATE TABLE IF NOT EXISTS users(
-id TEXT PRIMARY KEY,
-name TEXT        
+    cur.execute("""CREATE TABLE IF NOT EXISTS user (
+    id       TEXT PRIMARY KEY NOT NULL,
+    username TEXT
 );
+
+
+""")
+    
+    cur.execute("""CREATE TABLE IF NOT EXISTS task (
+    id          TEXT PRIMARY KEY,
+    userId      TEXT REFERENCES user (id),
+    title       TEXT,
+    description TEXT
+);
+
 
 """)
     
 
     conn.commit()
+
+
 
 
 
